c9e7f8f12d4cc74775aa1b51ef2bb0716736bc5c
Daniel059 <danzioki01@gmail.com>
2023-08-17 09:58:36 +0300

    Functions and Arrow function in Dart

--- 008_functions_and_return_types/main.dart
+++ 008_functions_and_return_types/main.dart
@@ -23,4 +23,32 @@ void main(List<String> args) {
     return "The Quotient of $a and $b is $quo";
    }
    print(Quotient(25, 5));//The Quotient of 25 and 5 is 5.0
+
+
+  //  return types functions
+  String Operations(){
+    num x = 15;
+    num y = 45;
+    num sum = x + y;
+    num product = x*y;
+    num quotient = x/y;
+    return "The summation of $x and $y is $sum and the product of $x and $y is $product while the quotient of $x and $y is $quotient";
+  }
+  print(Operations());//The summation of 15 and 45 is 60 and the product of 15 and 45 is 675 while the quotient of 15 and 45 is 0.3333333333333333
+
+  int mySum(int a, int b){
+    int sum = a + b;
+    return sum;
+  }
+  print(mySum(15, 45));//60
+
+  // Arrow functions in dart
+  int mySumB(int a, int b) => a + b;
+  print(mySumB(39, 56));//95
+
+  String salutation(String name,{String greetings = 'Hello'}) => '$greetings $name';
+  print(salutation("John Doe"));
+  //Hello John Doe
+
+
 }

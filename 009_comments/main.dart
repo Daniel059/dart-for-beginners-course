66233188d20d75fea1bdd028d6dfaf11a7ced62d
Daniel059 <danzioki01@gmail.com>
2023-08-17 14:56:04 +0300

    Comments in Dart

--- 009_comments/main.dart
+++ 009_comments/main.dart
@@ -1,1 +1,7 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+'''
+Are the sets of code or statements that are ignored by the compiler during program execution
+In dart single line comments are //
+while we use triple quotes as multiline comments
+''';
+}

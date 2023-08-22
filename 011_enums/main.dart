66dd98bc700eab5f30ed28a3343bc76188f10328
Daniel059 <danzioki01@gmail.com>
2023-08-22 12:54:39 +0300

    Git enums

--- 011_enums/main.dart
+++ 011_enums/main.dart
@@ -1,1 +1,24 @@
-void main(List<String> args) {}
+
+
+void main(List<String> args) {
+  // Enums in Dart
+// Enums are type of data type which contains fixed set of constant data types
+
+// Enum
+  List<Nilotes> nilepeople = Nilotes.values;
+  for (var i in nilepeople) {
+    print(i);
+  }
+  for (var i in nilepeople) {
+    print(i.name);
+  }
+
+}
+enum Nilotes {
+  Luo,
+  Maasai,
+  Nandi,
+  Turgen,
+  Turkana,
+}
+
